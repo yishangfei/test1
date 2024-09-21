ca54e2a68dd01193ef3d2aa6be5f3c0e8437491d
genius <yishangfei.me@gmail.com>
2024-09-21 10:27:14 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path/path.dart' as path;
+import 'package:image_picker/image_picker.dart';
+import 'package:photo_manager/photo_manager.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  void _pickVideo() async {
+    final PermissionState ps = await PhotoManager.requestPermissionExtend();
+    if (ps.isAuth) {
+
+      final XFile? file = await ImagePicker().pickVideo(source: ImageSource.gallery);
+      if (mounted && file != null) {
+        File result = File(file.path);
+        var entity = await PhotoManager.editor.saveVideo(
+          result,
+          title: path.basename(result.path),
+        );
+        debugPrint('original file===${file.path}');
+        debugPrint('assetEntity===${entity!.title}===${await entity.file}');
+      }
+    } else {
+      debugPrint('permission denied');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text("Click on the button to select video"),
+            ElevatedButton(
+              onPressed: _pickVideo,
+              child: const Text("Pick Video From Gallery"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
